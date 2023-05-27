393594f8ab360dbce39f958b229a15e628c1f704
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-05-28 02:51:49 +0530

    Update index.js

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,16 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
-
+let firstCard = 6
+let secondCard = 9
 // 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard
+
+if (sum < 21){
+    console.log("Do you want to draw a new card?")
+}
+else if (sum === 21){
+    console.log("BlackJack!")
+}
+else if (sum > 21){
+    console.log("You're out of the game!")
+}
