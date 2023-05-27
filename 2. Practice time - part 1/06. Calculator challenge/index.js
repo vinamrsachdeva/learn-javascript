e84d55e5934d885a34ba157a5d5e79441a1aeb61
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-05-28 02:09:10 +0530

    check

--- 2. Practice time - part 1/06. Calculator challenge/index.js
+++ 2. Practice time - part 1/06. Calculator challenge/index.js
@@ -11,4 +11,7 @@ document.getElementById("num2-el").textContent = num2
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
-
+function add(){
+    let result = num1 + num2
+    document.getElementById("sum-el").textContent = "Sum: " + result
+}
